5cb5f4b84369936b3c50b9619960ec8b25b330a0
Ubuntu <ubuntu@ip-172-31-33-17.ap-south-1.compute.internal>
2023-01-19 16:22:38 +0000

    My-Project1 file added

---
+++ src/project1/equalno.java
@@ -0,0 +1,22 @@
+package project1;
+
+public class equalno 
+{
+
+	public static void main(String[] args)
+	{
+		int a=10; 
+		int b=10; 
+		int c=10;
+	if(a==b && a==c)
+	{
+		System.out.println("all no are equal");
+	}
+		
+	}
+}
+
+		
+	
+
+
---
+++ src/project1/greaternum.java
@@ -0,0 +1,27 @@
+package project1;
+
+public class greaternum {
+
+	public static void main(String[] args) {
+		int a=10; 
+	    int b=20;
+		int c=30;
+		
+			if(a>b && a>c)
+			{
+			System.out.println("a is greater number");
+		    }
+			else if(b>c && b>a)
+			{
+	         System.out.println("a is greater number");
+			}
+			else
+			{
+			System.out.println("C is greater number");
+			
+		    }
+		
+
+	}
+
+}
